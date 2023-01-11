bdc0b5700269c1a6a7c3f12c4f4e0937c0093fbd
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2023-01-11 06:31:04 -0600

    Update prime_generator.sh

--- prime_generator.sh
+++ prime_generator.sh
@@ -2,12 +2,12 @@
 
 generate_random() {
 
-        head -c 256 /dev/urandom | xxd -p -u -c 256 | bc | tr -d '[:space:]\\'
+        head -c 256 /dev/urandom | xxd -p -u -c 256 | tr -d '[:space:]\\'
 }
 
 p="$(generate_random)"
 q="$(generate_random)"
 
-n=$(echo "${p} * ${q}" | bc | tr -d '[:space:]\\')
+n=$(echo "obase=16;ibase=16; ${p} * ${q}" | bc | tr -d '[:space:]\\')
 
-echo "$n"
+echo "$n" 
