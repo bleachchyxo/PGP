876bcdce8b4f3d39ffeb4942721ef0df22b2f73e
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2023-01-12 11:07:24 -0600

    Update prime_generator.sh

--- prime_generator.sh
+++ prime_generator.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-generate_random() {
+hexadecimalGenerator() {
 
         head -c 256 /dev/urandom | xxd -p -u -c 256 | tr -d '[:space:]\\'
+
 }
 
-p="$(generate_random)"
-q="$(generate_random)"
+witnessGenerator() {
 
+        witnessLimit=$(echo "obase=16;ibase=16; ${n} - 2" | bc | tr -d '[:space:]\\')
+        randomHexNumber="$(cat /dev/urandom | tr -dc '0-9' | head -c $((${#n} + 1)))"
+        randomModulus="$(echo "obase=16;ibase=16; ${randomHexNumber} % ${witnessLimit}" | bc | tr -d '[:space:]\\')"
+
+        echo "obase=16;ibase=16; ${randomModulus} + 2" | bc | tr -d '[:space:]\\'
+
+}
+
+p="$(hexadecimalGenerator)"
+q="$(hexadecimalGenerator)"
 n=$(echo "obase=16;ibase=16; ${p} * ${q}" | bc | tr -d '[:space:]\\')
-
-echo "$n" 
+witness="$(witnessGenerator)"
+predecessor="$(echo "obase=16;ibase=16; ${n} - 1" | bc | tr -d '[:space:]\\')"
